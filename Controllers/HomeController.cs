7297496a792f9dfa67a3ac67bb9e98e439b82e2b
Roman Dzygun <dzygun-roman@mail.ru>
2022-07-22 09:55:55 +0300

    LanguageFeatures

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,8 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name ?? "<No Name>";
                 decimal? price = p?.Price ?? 0;
                 string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",name,price,relatedName));
+                //results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",name,price,relatedName));//классика
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");//интерполяция строк
             }
             return View(results);
         }
